25b50b4458a9f223fc1371a3158364fd553f9136
Gabriel Martin Guillergan <gabrielmartin.guillergan@perpetual.edu.ph>
2024-08-23 21:40:40 +0800

    Added comments explaining the each function

--- Form1.vb
+++ Form1.vb
@@ -1,23 +1,28 @@
 ﻿Public Class BMICalculator
+    ' BMI Calculator
+    ' Gabriel Martin Guillergan (jeonnotcool)
+    ' Licensed under GNU General Public License v3.0
+
+    ' Declaring Variables
     Dim h As Double
     Dim w As Double
     Dim hConverted As Double
     Dim t As Double
 
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtWeight.TextChanged
-
-    End Sub
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles calculate.Click
+        ' Sets the variables to the values of the textboxes
         w = txtWeight.Text
         h = txtHeight.Text
+        ' Converts the height to meters
         hConverted = h / 100
 
+        ' BMI Formula = weight / (height * height)
         t = w / (hConverted * hConverted)
 
+        ' Format the value of t as a floating-point number with two decimal places.
         calculated.Text = String.Format("{0:f}", t)
 
+        ' If statements to determine the status of the user
         If t < 18.5 Then
             Status.BackColor = Color.SkyBlue
             Status.Text = "You are underweight."
@@ -35,6 +40,7 @@
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles reset.Click
+        ' Clears the textboxes and labels
         calculated.Text = ""
         txtHeight.Clear()
         txtWeight.Clear()
@@ -43,14 +49,13 @@
     End Sub
 
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        ' Exits the application
         Application.Exit()
     End Sub
 
     Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        ' Shows the AboutApp form
         AboutApp.Show()
     End Sub
 
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
 End Class
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # BMI-Calculator
 This is a project for Computer Education 10. A simple BMI calculator written on Visual Basic.
 
+For the project, I have created a simple BMI calculator that calculates the BMI of a person based on their weight and height. 
+The program also provides a brief description of the BMI value and the health status of the person.
+
+I have also added comments to the code to explain the purpose of each line of code so that it is easy to understand.s
+
 This project is licensed under the GNU General Public License v3.0
